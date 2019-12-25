f065039a002f4d6c8a6e1fd5e937b71452645df7
Vladimirov Stanislav <zstanislaw211@gmail.com>
2019-12-25 17:51:57 +0300

    add message

---
+++ src/main/java/ru/sv/spring7hibernate/conf.java
@@ -0,0 +1,16 @@
+package ru.sv.spring7hibernate;
+
+public class conf {
+	/*
+	git pull
+	
+	
+	cd C:/Projects/spring7hibernate
+	git add .
+	git commit -m "add message"	
+	git push -u origin master
+	
+	##git push --force -u origin master
+	#git remote add origin git@github.com:vladimirovstanislaw/spring7hibernate.git
+	*/
+}
